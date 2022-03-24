c662478551642377c0ad7905eec1627d1f812466
Ziuan-Xiang <90587414+Ziuan-Xiang@users.noreply.github.com>
2022-03-24 15:37:10 -0400

    Add files via upload

---
+++ index_copy/LF1_index_photos.py
@@ -0,0 +1,100 @@
+import json
+import boto3
+import os
+import sys
+import uuid
+import requests
+from datetime import *
+from requests_aws4auth import AWS4Auth
+
+host = 'https://search-myphotos-qxrhoxw3cn6hgmyy7ilju4slae.us-east-1.es.amazonaws.com'
+region = 'us-east-1'
+
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+
+def get_url(index, type):
+    url = host + '/' + index + '/' + type
+    return url
+
+
+def lambda_handler(event, context):
+    headers = { "Content-Type": "application/json" }
+    rek = boto3.client('rekognition')
+    
+    # bucket = 'store2photos'
+    # key = 'dog1.png'
+    
+    # labels = rek.detect_labels(
+    #         Image={
+    #             'S3Object': {
+    #                 'Bucket': bucket,
+    #                 'Name': key
+    #             }
+    #         },
+    #         MaxLabels=10
+    #         )
+    # print(labels)
+    
+    # get the image information from S3
+    for record in event['Records']:
+        print(record['s3'])
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        size = record['s3']['object']['size'] # up to 5MB
+        
+        # detect the labels of current image
+        labels = rek.detect_labels(
+            Image={
+                'S3Object': {
+                    'Bucket': bucket,
+                    'Name': key
+                }
+            },
+            MaxLabels=10
+        )
+        
+    # prepare JSON object
+    obj = {}
+    obj['objectKey'] = key
+    obj["bucket"] = bucket
+    obj["createdTimestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    obj["labels"] = []
+        
+    # get custom label from s3
+    s3 = boto3.client('s3')
+    headObject = s3.head_object(Bucket=bucket, Key=key)
+    metaData = headObject['Metadata']
+    print(metaData)
+    if len(metaData) != 0:
+        custom_label = metaData['customlabels']
+        print(custom_label)
+        obj["labels"].append(custom_label)
+    
+    
+    
+    for label in labels['Labels']:
+        obj["labels"].append(label['Name'])
+    
+    print(obj)
+    
+    
+    
+    # post the JSON object into ElasticSearch, _id is automaticlly increased
+    url = get_url('myphotos', 'Photo')
+    obj = json.dumps(obj)
+    req = requests.post(url, data=obj, headers=headers, auth=awsauth)
+        
+    print("Success: ", req.json())
+    
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps("Image labels have been successfully detected!")
+    }
---
+++ search_copy/LF2_search_photos.py
@@ -0,0 +1,91 @@
+import json
+import boto3
+import os
+import sys
+import uuid
+import time
+import requests
+import logging
+
+from requests_aws4auth import AWS4Auth
+
+host = 'https://search-myphotos-qxrhoxw3cn6hgmyy7ilju4slae.us-east-1.es.amazonaws.com'
+region = 'us-east-1'
+
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+
+
+def get_url(es_index, es_type, keyword):
+    url = host + '/' + es_index + '/' + es_type + '/_search?q=' + keyword.lower()
+    return url
+
+def lambda_handler(event, context):
+	
+	headers = { "Content-Type": "application/json" }
+	lex = boto3.client('lex-runtime')
+	
+	# print(event)
+	# query = "show me dogs and cats"
+	query = event["queryStringParameters"]["q"]
+	# query = event["q"]
+	print(query)
+	query = query.replace("%20", " ")
+	
+	lex_response = lex.post_text(
+		botName='SearchPhotos',
+		botAlias='SearchPhotos',
+		userId='jinyaowu1',
+		inputText=query
+	)
+	
+	print("lex_response:", json.dumps(lex_response))
+	
+	slots = lex_response['slots']
+	
+	print("slots:",slots)
+	
+	img_list = []
+	for i, tag in slots.items():
+		if tag:
+			url = get_url('myphotos', 'Photo', tag)
+			print("ES URL --- {}".format(url))
+
+			es_response = requests.get(url, headers=headers, auth=awsauth).json()
+			print("ES RESPONSE --- {}".format(json.dumps(es_response)))
+
+			es_src = es_response['hits']['hits']
+			print("ES HITS --- {}".format(json.dumps(es_src)))
+
+			for photo in es_src:
+				labels = [word.lower() for word in photo['_source']['labels']]
+				if tag in labels:
+					objectKey = photo['_source']['objectKey']
+					img_url = 'https://store2photos.s3.amazonaws.com/' + objectKey
+					img_list.append(img_url)
+
+	# url = get_url('myphotos', 'Photo', 'cat')
+	# resp = requests.get(url, auth=awsauth)  # requests.get, post, and delete have similar syntax
+	# resp = resp.json()
+	# print(resp)
+	
+	if img_list:
+		return {
+			'statusCode': 200,
+			'headers': {
+				"Access-Control-Allow-Origin": "*",
+				'Content-Type': 'application/json'
+			},
+			'body': json.dumps(img_list)
+		}
+	else:
+		return {
+			'statusCode': 200,
+			'headers': {
+				"Access-Control-Allow-Origin": "*",
+				'Content-Type': 'application/json'
+			},
+			'body': json.dumps("No such photos.")
+		}
